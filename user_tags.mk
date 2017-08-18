b07556b99b13338c22b37237e12833ad134e390a
Gary Bisson <gary.bisson@boundarydevices.com>
2017-08-18 19:20:18 +0200

    user_tags.mk: include all necessary packages/files
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- user_tags.mk
+++ user_tags.mk
@@ -4,7 +4,7 @@
 # Add all Sierra specific modules to the list of modules grandfathered to use a
 # user tag.  Technically, we are not supposed to add new modules to the list of
 # grandfathered modules, but this is the easiest way to manage Sierra specific
-# modules.  
+# modules.
 #
 #
 # Copyright (C) 2011 Sierra Wireless, Inc.
@@ -44,4 +44,26 @@ PRODUCT_PACKAGES += \
     swiomadm \
 	ash \
 	rild
-	
+
+# telephony.mk packages
+PRODUCT_PACKAGES += \
+	CarrierConfig \
+	Dialer \
+	CallLogBackup \
+	CellBroadcastReceiver \
+	EmergencyInfo \
+	rild
+
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.carrier=unknown \
+	persist.sierra.sim_ready_delay=15 \
+	ro.sierra.voice=1 \
+	ro.com.android.mobiledata=true \
+	ro.telephony.default_network=10
+
+PRODUCT_COPY_FILES += \
+	frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml \
+	frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+	hardware/sierra/etc/init.config.ip:system/bin/init.config.ip \
+	hardware/sierra/etc/init.dhcpcd:system/bin/init.dhcpcd \
+	hardware/sierra/etc/init.sierra.rc:root/init.sierra.rc
